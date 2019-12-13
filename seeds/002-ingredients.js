18ceb90704d385eb0b9961dda568a1401e0d7f36
Kerri-Ann <kaybee1jam@yahoo.com>
2019-12-12 21:04:09 -0500

    original migration

---
+++ seeds/002-ingredients.js
@@ -0,0 +1,16 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('ingredients').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('ingredients').insert([
+        {name: 'Green Apple'},
+        {name: 'Flour'},
+        {name: 'Chicken'},
+        {name: 'Jerk Seasoning'},
+        {name: 'Brown Sugar'}
+
+      ]);
+    });
+};
